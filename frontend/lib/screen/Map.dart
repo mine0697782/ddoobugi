6e2ce7b4d5a200c29af8a9342d6840a446a933c6
gyur2 <66067610+gyur2@users.noreply.github.com>
2024-07-23 14:16:50 +0900

    [Feat] add Map.dart

---
+++ frontend/lib/screen/Map.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class mapScreen extends StatefulWidget {
+  const mapScreen({super.key});
+
+  @override
+  State<mapScreen> createState() => _mapScreenState();
+}
+
+class _mapScreenState extends State<mapScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Image.asset(
+            '../assets/images/logo.png',
+            scale: 6,
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
