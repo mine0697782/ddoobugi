01773a13b89d1b1d3b9c35dc2c1457a206c42935
gyur2 <66067610+gyur2@users.noreply.github.com>
2024-07-25 11:40:19 +0900

    [Design] fix down button design
    delete clock

--- frontend/lib/screen/Map.dart
+++ frontend/lib/screen/Map.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:frontend/components/app_theme.dart';
 import 'package:frontend/screen/chat.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -18,9 +19,12 @@ class _MapScreenState extends State<MapScreen> {
   bool isExpanded = false;
   bool _myLocationEnabled = false;
   final LatLng _center = const LatLng(37.6098, 127.0737);
+  final List<LatLng> polylineCoordinates = [];
+  final Set<Polyline> _polylines = {};
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _addRoute();
   }
 
   void _updateTime() {
@@ -47,6 +51,66 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
+  Future<void> _addRoute() async {
+    final startLat = 37.6098;
+    final startLng = 127.0737;
+    final endLat = 37.5665; // Destination latitude
+    final endLng = 126.9780; // Destination longitude
+    final apiKey = 'AIzaSyD-rsNPFz79dDdN00dd-IQTXgppFHjp3N8';
+
+    final url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$startLat,$startLng&destination=$endLat,$endLng&key=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    final data = json.decode(response.body);
+
+    if (data['routes'].isNotEmpty) {
+      final points = data['routes'][0]['overview_polyline']['points'];
+      final List<LatLng> result = _decodePolyline(points);
+      setState(() {
+        polylineCoordinates.addAll(result);
+        _polylines.add(Polyline(
+          polylineId: PolylineId('route'),
+          points: polylineCoordinates,
+          color: Colors.blue,
+          width: 5,
+        ));
+      });
+    }
+  }
+
+  List<LatLng> _decodePolyline(String encoded) {
+    List<LatLng> polyline = [];
+    int index = 0;
+    int len = encoded.length;
+    int lat = 0;
+    int lng = 0;
+
+    while (index < len) {
+      int b, shift = 0, result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1F) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1F) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lng += dlng;
+
+      polyline.add(LatLng(lat / 1E5, lng / 1E5));
+    }
+
+    return polyline;
+  }
+
   @override
   void initState() {
     super.initState();
@@ -62,9 +126,9 @@ class _MapScreenState extends State<MapScreen> {
             setState(() {
               isExpanded = false;
             });
-            return false; // Back button action is handled
+            return false;
           }
-          return true; // Default back button action
+          return true;
         },
         child: Scaffold(
           appBar: const CustomAppBar(),
@@ -78,89 +142,112 @@ class _MapScreenState extends State<MapScreen> {
                 ),
                 myLocationEnabled: _myLocationEnabled,
                 myLocationButtonEnabled: true,
+                polylines: _polylines,
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  width: 300,
-                  height: 70,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: isExpanded
-                            ? [
-                                IconButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const Chat()));
-                                  },
-                                  icon: const Icon(Icons.search),
-                                  color: Colors.black,
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Icons.person),
-                                  color: Colors.black,
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    statedialog(context, 70);
-                                  },
-                                  icon: const Icon(Icons.stop),
-                                  color: Colors.red,
-                                ),
-                                IconButton(
-                                  onPressed: _toggleExpansion,
-                                  icon: const Icon(Icons.menu),
-                                  color: Colors.black,
-                                ),
-                              ]
-                            : [
-                                FloatingActionButton(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(100)),
-                                  onPressed: _updateTime,
-                                  backgroundColor: Colors.white,
-                                  heroTag: "refreshButton",
-                                  elevation: 0,
-                                  child: const Icon(Icons.refresh,
-                                      color: Colors.black),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.white,
-                                    elevation: 0,
-                                  ),
-                                  child: Text(
-                                    '${selectedDate.hour}:${selectedDate.minute}:${selectedDate.second}',
-                                    style: const TextStyle(color: Colors.black),
-                                  ),
-                                ),
-                                FloatingActionButton(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(100)),
-                                  onPressed: _toggleExpansion,
-                                  backgroundColor: Colors.white,
-                                  heroTag: "menuButton",
-                                  elevation: 0,
-                                  child: const Icon(Icons.menu,
-                                      color: Colors.black),
-                                ),
-                              ],
+              isExpanded
+                  ? Align(
+                      alignment: Alignment.bottomLeft,
+                      child: SizedBox(
+                          width: 300,
+                          height: isExpanded ? 95 : 70,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 2,
+                                      blurRadius: 5,
+                                      offset: Offset(0, 10)),
+                                ],
+                              ),
+                              child: Container(
+                                margin: EdgeInsets.all(8),
+                                child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      IconButton(
+                                        onPressed: _toggleExpansion,
+                                        icon: const Icon(Icons.menu),
+                                        color: Colors.black,
+                                      ),
+                                      Column(
+                                        children: [
+                                          IconButton(
+                                            onPressed: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const Chat()));
+                                            },
+                                            icon: const Icon(Icons.search),
+                                            color: Colors.black,
+                                          ),
+                                          Text(
+                                            "AI",
+                                            style: TextStyle(
+                                                fontFamily: "Hanbit",
+                                                fontSize: 12),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          IconButton(
+                                            onPressed: () {},
+                                            icon: const Icon(Icons.person),
+                                            color: Colors.black,
+                                          ),
+                                          Text(
+                                            "MyPage",
+                                            style: TextStyle(
+                                                fontFamily: "Hanbit",
+                                                fontSize: 12),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          IconButton(
+                                            onPressed: () {
+                                              statedialog(context, 70);
+                                            },
+                                            icon: const Icon(Icons.stop),
+                                            color: Colors.red,
+                                          ),
+                                          Text(
+                                            "stop",
+                                            style: TextStyle(
+                                                fontFamily: "Hanbit",
+                                                fontSize: 12),
+                                          )
+                                        ],
+                                      ),
+                                    ]),
+                              ),
+                            ),
+                          )),
+                    )
+                  : Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(12, 12, 12, 19),
+                        child: FloatingActionButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100)),
+                          onPressed: _toggleExpansion,
+                          backgroundColor: Colors.white,
+                          heroTag: "menuButton",
+                          elevation: 3,
+                          child: const Icon(Icons.menu, color: Colors.black),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ),
             ],
           ),
         ),
